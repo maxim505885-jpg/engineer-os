cc513828f7bdc6427bab0bf343cca0ab84f3f7cf
maxim505885-jpg <maxim505885@gmail.com>
2026-09-23 20:47:05 +0300

    feat(core): expose unified engineering runner

--- engineering/core/__init__.py
+++ engineering/core/__init__.py
@@ -2,7 +2,8 @@
 
 from .contracts import AgentResult, AgentStatus, EngineerTask, MaterialRef, SpecialistTask
 from .engineer_core import EngineerCore
+from .runner import EngineerRunner, RunSummary
 
 __all__ = [
-    "AgentResult", "AgentStatus", "EngineerTask", "MaterialRef", "SpecialistTask", "EngineerCore"
+    "AgentResult", "AgentStatus", "EngineerTask", "MaterialRef", "SpecialistTask", "EngineerCore", "EngineerRunner", "RunSummary"
 ]
