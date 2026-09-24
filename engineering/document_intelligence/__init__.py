9ff63b52ce12c0da925b1f82feccc5cb53db23d1
maxim505885-jpg <maxim505885@gmail.com>
2026-09-24 20:23:07 +0300

    feat: export evidence context

--- engineering/document_intelligence/__init__.py
+++ engineering/document_intelligence/__init__.py
@@ -1,5 +1,18 @@
 from .docx import DocxTextExtractor, DocumentExtractionError, ExtractedDocument, ExtractedTable, ExtractedBlock
 from .chunking import DocumentChunker, DocumentChunk
 from .pipeline import DocumentIntelligence, DocumentIntelligenceResult
+from .context import EvidenceContext, EvidenceContextError
 
-__all__ = ["DocxTextExtractor", "DocumentExtractionError", "ExtractedDocument", "ExtractedTable", "ExtractedBlock", "DocumentChunker", "DocumentChunk", "DocumentIntelligence", "DocumentIntelligenceResult"]
+__all__ = [
+    "DocxTextExtractor",
+    "DocumentExtractionError",
+    "ExtractedDocument",
+    "ExtractedTable",
+    "ExtractedBlock",
+    "DocumentChunker",
+    "DocumentChunk",
+    "DocumentIntelligence",
+    "DocumentIntelligenceResult",
+    "EvidenceContext",
+    "EvidenceContextError",
+]
