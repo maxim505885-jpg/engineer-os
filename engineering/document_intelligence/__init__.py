fec8a2a81d1f9e0e476b6524be82c65fabea95db
maxim505885-jpg <maxim505885@gmail.com>
2026-09-24 18:09:06 +0300

    feat: expose document ingestion pipeline

--- engineering/document_intelligence/__init__.py
+++ engineering/document_intelligence/__init__.py
@@ -1,4 +1,5 @@
 from .docx import DocxTextExtractor, DocumentExtractionError, ExtractedDocument, ExtractedTable, ExtractedBlock
 from .chunking import DocumentChunker, DocumentChunk
+from .pipeline import DocumentIntelligence, DocumentIntelligenceResult
 
-__all__ = ["DocxTextExtractor", "DocumentExtractionError", "ExtractedDocument", "ExtractedTable", "ExtractedBlock", "DocumentChunker", "DocumentChunk"]
+__all__ = ["DocxTextExtractor", "DocumentExtractionError", "ExtractedDocument", "ExtractedTable", "ExtractedBlock", "DocumentChunker", "DocumentChunk", "DocumentIntelligence", "DocumentIntelligenceResult"]
