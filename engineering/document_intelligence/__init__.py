ffc9d07c86e16a57114f2340b546df7b8d00cce3
maxim505885-jpg <maxim505885@gmail.com>
2026-09-24 18:06:03 +0300

    feat: expose document intelligence chunking contracts

--- engineering/document_intelligence/__init__.py
+++ engineering/document_intelligence/__init__.py
@@ -1,3 +1,4 @@
-from .docx import DocxTextExtractor, DocumentExtractionError, ExtractedDocument, ExtractedTable
+from .docx import DocxTextExtractor, DocumentExtractionError, ExtractedDocument, ExtractedTable, ExtractedBlock
+from .chunking import DocumentChunker, DocumentChunk
 
-__all__ = ["DocxTextExtractor", "DocumentExtractionError", "ExtractedDocument", "ExtractedTable"]
+__all__ = ["DocxTextExtractor", "DocumentExtractionError", "ExtractedDocument", "ExtractedTable", "ExtractedBlock", "DocumentChunker", "DocumentChunk"]
