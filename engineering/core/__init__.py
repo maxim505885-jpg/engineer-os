5e22447203fd7dc9f0651a4d76572a64d52e1831
maxim505885-jpg <maxim505885@gmail.com>
2026-09-24 08:28:16 +0300

    feat: export task engine primitives

--- engineering/core/__init__.py
+++ engineering/core/__init__.py
@@ -3,7 +3,8 @@
 from .contracts import AgentResult, AgentStatus, EngineerTask, MaterialRef, SpecialistTask
 from .engineer_core import EngineerCore
 from .runner import EngineerRunner, RunSummary
+from .task_engine import TaskEngine, TaskRecord, TaskStatus
 
 __all__ = [
-    "AgentResult", "AgentStatus", "EngineerTask", "MaterialRef", "SpecialistTask", "EngineerCore", "EngineerRunner", "RunSummary"
+    "AgentResult", "AgentStatus", "EngineerTask", "MaterialRef", "SpecialistTask", "EngineerCore", "EngineerRunner", "RunSummary", "TaskEngine", "TaskRecord", "TaskStatus"
 ]
